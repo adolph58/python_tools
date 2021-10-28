755c5457f00989c55481bf75649a7b8c61d93b0f
Arte <jiangtao.cheng@orderplus.com>
2021-10-28 10:16:51 +0800

    增加 i

--- pwd_generate.py
+++ pwd_generate.py
@@ -1,12 +1,12 @@
 import random
 
-repo = ['2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z',
+repo = ['2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z',
         'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 def get_pwd():
     pwd = ''
     for i in range(1, 17):
-        num = random.randint(0, 54)
+        num = random.randint(0, 55)
         pwd += repo[num]
     return pwd
 
